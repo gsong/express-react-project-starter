92931c5b86dce7a5de9adfe458108e13f83084c7
George Song <george@monozuku.com>
2021-03-18 14:38:50 -0700

    feat: set up basic Express API server

---
+++ server/server.mjs
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const port = 5000;
+
+app.get("/tasks", (req, res) => {
+  res.json([
+    { id: 1, name: "End white supremacy" },
+    { id: 2, name: "Enact living wage" },
+  ]);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
