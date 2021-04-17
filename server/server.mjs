1d8e705505dd0182b763213874ce96f2eaea799b
George Song <george@monozuku.com>
2021-04-16 19:59:30 -0700

    feat: set cache control for static files

--- server/server.mjs
+++ server/server.mjs
@@ -1,4 +1,5 @@
 import express from "express";
+import mime from "mime-types";
 
 import * as db from "./db.mjs";
 
@@ -6,7 +7,14 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 process.env?.SERVE_REACT?.toLowerCase() === "true" &&
-  app.use(express.static("/app"));
+  app.use(
+    express.static("/app", {
+      maxAge: "1d",
+      setHeaders: (res, path) =>
+        ["application/json", "text/html"].includes(mime.lookup(path)) &&
+        res.setHeader("Cache-Control", "public, max-age=0"),
+    })
+  );
 
 app.get("/api/ping", (request, response) =>
   response.json({ response: "pong" })
