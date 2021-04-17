36479c57b7a769ab3420cd303fb1549ea9cae04a
George Song <george@monozuku.com>
2021-04-16 21:47:22 -0700

    build: add Heroku PostgreSQL

--- server/db.mjs
+++ server/db.mjs
@@ -1,14 +1,7 @@
 import dotenv from "dotenv";
 import pgp from "pg-promise";
 
-dotenv.config({ path: "../.env" });
-
-const db = pgp()({
-  user: "postgres",
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-  port: 5442,
-});
+const db = initDb();
 
 export const getTasks = async () => await db.any("SELECT * FROM tasks");
 
@@ -18,3 +11,24 @@ export const addTask = async (name) =>
       name,
     ])
   )[0];
+
+function initDb() {
+  let connection;
+
+  if (process.env.DATABASE_URL === undefined) {
+    dotenv.config({ path: "../.env" });
+    connection = {
+      user: "postgres",
+      database: process.env.POSTGRES_DB,
+      password: process.env.POSTGRES_PASSWORD,
+      port: 5442,
+    };
+  } else {
+    connection = {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    };
+  }
+
+  return pgp()(connection);
+}
