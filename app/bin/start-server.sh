2626010238f766c247a1d2eaf1d141d56ff50d0d
George Song <george@monozuku.com>
2021-04-19 12:21:25 -0700

    build: set up local service startup dependencies

---
+++ app/bin/start-server.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+until curl -sSf http://localhost:${PORT:-4000}/api/ping > /dev/null
+do
+  sleep 5
+done
+
+npx react-scripts start
---
+++ server/bin/start-server.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+until docker-compose exec -u postgres db pg_isready > /dev/null
+do
+  sleep 5
+done
+
+npx nodemon server.mjs
