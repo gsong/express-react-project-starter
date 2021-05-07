f3282757d95356122048e06e37f883ea5687c927
George Song <george@monozuku.com>
2021-05-07 09:23:18 -0700

    feat: add title via envvar

--- app/src/App.js
+++ app/src/App.js
@@ -13,6 +13,7 @@ const App = () => {
 
   return (
     <main className="App">
+      <h1>{process.env.REACT_APP_TITLE}</h1>
       <TaskList tasks={tasks} />
       <AddTask loadTasks={loadTasks} />
     </main>
