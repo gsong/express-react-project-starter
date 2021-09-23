d20f60f30c5e0cfd6c936c51687af90cdb09328b
George Song <george@monozuku.com>
2021-09-23 13:39:45 -0700

    fix: remove react-router since it's not needed for Eventonica

--- app/src/App.js
+++ app/src/App.js
@@ -1,39 +1,17 @@
 import * as React from "react";
 
-import { Routes, Route, Link } from "react-router-dom";
-
 import Events from "./Events";
 import Users from "./Users";
 
-const App = () => (
-  <>
-    <nav>
-      <Link to="/">Home</Link> | <Link to="dashboard">Dashboard</Link>
-    </nav>
-    <main>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </main>
-  </>
-);
-
-const Home = () => {
+const App = () => {
   const [selectedUser, setSelectedUser] = React.useState();
 
   return (
-    <>
+    <main>
       <Users {...{ selectedUser, setSelectedUser }} />
       <Events {...{ selectedUser }} />
-    </>
+    </main>
   );
 };
-
-const Dashboard = () => (
-  <>
-    <h1>Dashboard</h1>
-  </>
-);
 
 export default App;
