fbaa995998a55ef20358ad533f4f13d5fc63d7a7
George Song <george@damacy.net>
2021-10-12 02:53:05 -0700

    feat: associate tasks to users

--- server/src/db.mjs
+++ server/src/db.mjs
@@ -6,10 +6,19 @@ load_dotenv_if_exists();
 
 const db = initDb();
 
-export const getTasks = () => db.any("SELECT * FROM tasks");
+export const getTasks = (sub) =>
+  db.any(
+    "SELECT tasks.* FROM tasks LEFT JOIN users on user_id=users.id WHERE sub=$<sub>",
+    { sub },
+  );
 
-export const addTask = (name) =>
-  db.one("INSERT INTO tasks(name) VALUES($<name>) RETURNING *", { name });
+export const addTask = (sub, name) =>
+  db.one(
+    `INSERT INTO tasks(user_id, name)
+      VALUES((SELECT id FROM users WHERE sub=$<sub>), $<name>)
+      RETURNING *`,
+    { sub, name },
+  );
 
 export const addOrUpdateUser = (user) =>
   db.one(
@@ -18,8 +27,7 @@ export const addOrUpdateUser = (user) =>
       ON CONFLICT (sub) DO
         UPDATE SET given_name = $<given_name>, family_name = $<family_name>,
           picture = $<picture>, email=$<email>
-      RETURNING *
-    `,
+      RETURNING *`,
     user,
   );
 
--- server/src/taskRouter.mjs
+++ server/src/taskRouter.mjs
@@ -5,13 +5,13 @@ import * as db from "./db.mjs";
 const router = express.Router();
 
 router.get("/", async (request, response) => {
-  const tasks = await db.getTasks();
+  const tasks = await db.getTasks(request.user.sub);
   response.json(tasks);
 });
 
 router.use(express.json());
 router.post("/", async (request, response) => {
-  const task = await db.addTask(request.body.name);
+  const task = await db.addTask(request.user.sub, request.body.name);
   response.status(201).json(task);
 });
 
