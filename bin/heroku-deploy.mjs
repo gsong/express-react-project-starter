2841d1659e52b74e7fef823e6ac7a6ae901fa3ed
George Song <george@damacy.net>
2021-10-21 12:18:34 -0700

    build: local Heroku deploy that takes into account `app/.env`

---
+++ bin/heroku-deploy.mjs
@@ -0,0 +1,32 @@
+import fs from "fs";
+import path from "path";
+import { URL } from "url";
+import { spawnSync } from "child_process";
+
+import dotenv from "dotenv";
+
+const DOTENV_FILE = path.join(
+  new URL(".", import.meta.url).pathname,
+  "../app/.env",
+);
+
+let args;
+
+if (fs.existsSync(DOTENV_FILE)) {
+  args = dotenv.parse(fs.readFileSync(DOTENV_FILE));
+}
+
+const argString = Object.entries(args)
+  .map(([key, value]) => `${key}="${value}"`)
+  .join(",");
+
+spawn("heroku container:login");
+spawn(
+  `DOCKER_DEFAULT_PLATFORM=linux/amd64 heroku container:push web --arg ${argString}`,
+);
+spawn("heroku container:release web");
+
+function spawn(cmd) {
+  console.info(cmd);
+  spawnSync(cmd, { shell: true, stdio: "inherit" });
+}
--- bin/heroku-deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-heroku container:login
-DOCKER_DEFAULT_PLATFORM=linux/amd64 heroku container:push web
-heroku container:release web
