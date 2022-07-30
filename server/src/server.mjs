Shaylan7/Assignments

feat: user management end-to-end

server/server.mjs
@@ -1,26 +1,12 @@
 import express from "express";
 import mime from "mime-types";
 
-import * as db from "./db.mjs";
+import userRoutes from "./users.mjs";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
-const tasks = express.Router();
-
-tasks.get("/", async (request, response) => {
-  const tasks = await db.getTasks();
-  response.json(tasks);
-});
-
-tasks.use(express.json());
-tasks.post("/", async (request, response) => {
-  const { name } = request.body;
-  const task = await db.addTask(name);
-  response.status(201).json(task);
-});
-
-app.use("/api/tasks", tasks);
+app.use("/api/users", userRoutes);
 
 app.get("/api/ping", (request, response) =>
   response.json({ response: "pong" }),

gsong/express-react-project-starter

server/src/server.mjs
@@ -1,11 +1,11 @@
 import express from "express";
 import mime from "mime-types";
 
-import taskRouter from "./taskRouter.mjs";
+import userRouter from "./userRouter.mjs";
 
 const app = express();
 
-app.use("/api/tasks", taskRouter);
+app.use("/api/users", userRouter);
 
 // Do not comment out or delete this end point. The React development server
 // won't start until it pings this end point successfully.