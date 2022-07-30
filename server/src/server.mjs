Shaylan7/Assignments

feat: add event handling to app 🥳

server/server.mjs
@@ -1,11 +1,13 @@
 import express from "express";
 import mime from "mime-types";
 
+import eventRoutes from "./events.mjs";
 import userRoutes from "./users.mjs";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use("/api/events", eventRoutes);
 app.use("/api/users", userRoutes);
 
 app.get("/api/ping", (request, response) =>

gsong/express-react-project-starter

server/src/server.mjs
@@ -1,10 +1,12 @@
 import express from "express";
 import mime from "mime-types";
 
+import eventRouter from "./eventRouter.mjs";
 import userRouter from "./userRouter.mjs";
 
 const app = express();
 
+app.use("/api/events", eventRouter);
 app.use("/api/users", userRouter);
 
 // Do not comment out or delete this end point. The React development server